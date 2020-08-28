fd033beefb3236409f123756c086f6e7cfe67c0c
ilias <iliasdavid@live.fr>
2020-08-28 10:45:15 +0200

    Add a component : ListShower
    This component is imported friom App.vue.
    
    It execute the API Request (with fetch) and transform it into JSON format.
    
    Foreach object into the result it will create a new ListObject. ListObject is in developement.

---
+++ src/components/ListShower.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="show" class="row">
+    <div class="col-2" v-if="isRequestOk" v-for="item in jsonContent" v-bind:key="item">
+      <ListObject country="item.issuing_country" ref="item.bti_ref" end="item.end_date"
+                  desc="item.description_fr" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListShower',
+  props: [ 'filter', 'search', 'limit' ],
+  data () {
+    return {
+      jsonContent: '',
+      isRequestOk: false
+    }
+  },
+  async created () {
+    let response = await fetch("https://api.customsbridge.ai/ebti?name=" + 
+                                            this.filter + "&search=" +
+                                            this.search + "&limit=" 
+                                            + this.limit);
+    if (response.ok) {
+      this.jsonContent = await response.json();
+      this.isRequestOk = true;
+    } else {
+      this.isRequestOk = false;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
