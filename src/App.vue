e41ad3d01df612b29a6f4c322e16d3d54c6a84c2
ilias <iliasdavid@live.fr>
2020-08-28 10:26:12 +0200

    Initialisation of the project, app.vue and main.js
    Start the project with an index.html, an App.vue file who cointains de main layout of the app.
    
    Add package.json to get a simple installation.
    
    Add main.js with bootstrap including.

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app m-0">
+    <div class="container-fluid">
+
+      <nav class="row p-2 color-light b-bottom">
+        <img src="" class="col-1 color-black"/>
+        <input v-model="model_filter_on_code" class="col-4 mr-3 b-bottom color-light" placeholder="Taper votre code nomenclature" type="text" />
+        <input v-model="model_search" class="col-6 b-bottom color-light" placeholder="Décrivez votre RTC" type="text" />
+      </nav>
+
+      <ListShower class="color-light" v-bind:filter="model_filter_on_code" v-bind:search="model_search" v-bind:limit="model_limit"/>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import ListShower from './components/ListShower'
+
+export default {
+  name: 'App',
+  components: {
+    ListShower
+  },
+  data () {
+    return {
+      isRequestOk: false,
+      model_limit: 100,
+      model_filter_on_code: '',
+      model_search: ''
+    }
+  }
+}
+</script>
+
+<style>
+.color-light {
+  background-color: #DEDFD7;
+}
+.color-black {
+  color: black;
+}
+.b-bottom {
+  border: none;
+  border-bottom: solid 1px gray;
+}
+</style>
